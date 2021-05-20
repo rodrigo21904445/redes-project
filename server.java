692dfd7208902244a7658a38949d7a4e4f2f5039
rodrigo21904445 <61628391+rodrigo21904445@users.noreply.github.com>
2021-05-20 17:26:49 +0100

    Add files via upload

--- server.java
+++ server.java
@@ -3,9 +3,51 @@ import java.io.*;
 
 public class server {
 
+	public static class ClientThread implements Runnable{
+		private Socket socket;
+		
+		public ClientThread(Socket socket){
+			this.socket = socket;
+		}
+
+		public void run() {
+			try {
+				// read input stream
+				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				// read output stream
+				PrintStream output = new PrintStream(socket.getOutputStream());
+				// put input in a buffer
+				String buffer = input.readLine();
+				// print the input
+				output.println(buffer);
+				// close socket
+				socket.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 
-	public static void public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+		// initialize socket
+		Socket socket = null;
+
+		// start at port 7142
+		ServerSocket server = new ServerSocket(7142);
+		System.out.println("Server started on port 7142...");
+
+		while(true) {
+			// waits for connection
+			// receive socket
+			socket = server.accept();
+
+			// create new thread for every connection made
+			Thread thread = new Thread(new ClientThread(socket));
+			
+			// execute thread
+			thread.start();
+		}
 	}
 }
